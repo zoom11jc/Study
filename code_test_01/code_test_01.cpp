6bd69636b72fbbd1247394d5073c49d7cc5f2eca
jungchan <jungchan1.kim@gmail.com>
2017-07-01 07:07:33 +0900

    Testing printf function

--- code_test_01/code_test_01.cpp
+++ code_test_01/code_test_01.cpp
@@ -1,11 +1,11 @@
-// code_test_01.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
 
-#include "stdafx.h"
 
+//My first main code
+
+#include <stdio.h>
 
 int main()
 {
-    return 0;
+	printf("Hello, jungchan kim\n");
 }
 
